ee8196fb36d21b11d538070d1c0b72b508ab2dc1
JJWJ <ambitionwins@gmail.com>
2021-08-03 13:52:25 -0700

    Fix animation in tailwind.config

---
+++ components/UI/ModalOverlay.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ModalOverlay = () => {
+    return (
+        <div className='fixed top-1/5-vh left-1/20 w-11/12 bg-white p-4 rounded-xl shadow-md z-30 animate-slide-down'>
+            
+        </div>
+    )
+}
+
+export default ModalOverlay
