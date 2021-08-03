bfd979109d7f11a1d3767c5833885834320b9bae
JJWJ <ambitionwins@gmail.com>
2021-08-03 13:52:55 -0700

    Fix animation in tailwind.config

---
+++ components/UI/ModalBackdrop.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ModalBackdrop = () => {
+    return (
+        <div className='fixed top-0 left-0 w-full h-screen z-20 bg-opacity-25 bg-black'>
+            
+        </div>
+    )
+}
+
+export default ModalBackdrop
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,30 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        'appear': '1s ease-out forwards'
+        'appear': 'appear 1s ease-out forwards',
+        'slide-down': 'slide-down 300ms ease-out forwards'
       },
       keyframes: {
         appear: {
           '0%': {transform: 'translateY(3rem)'},
           '100%': {transform: 'translateY(0)'},
+        },
+        'slide-down': {
+          '0%': {transform: 'translateY(-3rem)'},
+          '100%': {transform: 'translateY(0)'}
         }
       },
       boxShadow: {
         heavy: '0 1px 18px 10px rgba(0, 0, 0, 0.25)',
       },
+      inset: {
+        '1/5-vh': '20vh',
+        '2/5-vh': '40vh',
+        '3/5-vh': '60vh',
+        '4/5-vh': '80vh',
+        '1/20': '5%',
+        'calcLeft': 'calc(50% -20rem)'
+      }
     },
   },
   variants: {
