899710a5d739c714a752d0368ad76a76beff5a01
JJWJ <ambitionwins@gmail.com>
2021-08-06 19:32:46 -0700

    Add fetch and remove hard coded meals

--- components/Meals/AvailableMeals.tsx
+++ components/Meals/AvailableMeals.tsx
@@ -1,46 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from '../UI/Card';
 import MealItem from './MealItem';
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
+export interface IMeal {
+    id:          string;
+    description: string;
+    name:        string;
+    price:       number;
+}
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map(meal => <MealItem key={meal.id} id={meal.id} description={meal.description} mealName={meal.name} price={meal.price} />)
+  const [meals, setMeals] = useState<IMeal[]>([]);
 
-    return (
-        <section  className='max-w-5xl w-11/12 my-4 mx-auto animate-appear'>
-            <ul title='meals list' className='list-none m-0 p-0'>
-              <Card>
-                {mealsList}
-              </Card>
-            </ul>
-        </section>
-    )
+
+  useEffect(() => {
+      const fetchMeals = async () => {
+          const response = await fetch('https://react-food-app-88148-default-rtdb.firebaseio.com/meals.json')
+          const responseData = await response.json();
+
+          const loadedMeals: Array<IMeal> = [];
+
+          for(const key in responseData){
+              loadedMeals.push({
+                  id: key,
+                  name: responseData[key].name,
+                  description: responseData[key].description,
+                  price: responseData[key].price
+              })
+          }
+
+          setMeals(loadedMeals);
+      }
+      fetchMeals()
+  }, [])
+
+
+  const mealsList = meals.map(meal => <MealItem key={meal.id} id={meal.id} description={meal.description} mealName={meal.name} price={meal.price} />)
+
+  return (
+      <section  className='max-w-5xl w-11/12 my-4 mx-auto animate-appear'>
+          <ul title='meals list' className='list-none m-0 p-0'>
+            <Card>
+              {mealsList}
+            </Card>
+          </ul>
+      </section>
+  )
 }
 
 export default AvailableMeals
